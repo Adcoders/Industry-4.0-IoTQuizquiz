86a676084cd7d10fcf9473b57965a8c6a067de99
Adi-Pro <101057491+Adcoders@users.noreply.github.com>
2025-03-14 22:49:05 +0530

    Update week1.js

--- weeks/week1.js
+++ weeks/week1.js
@@ -1,13 +1,120 @@
 const week1Data = [
-    {
-      question: "What is the main concept of Industry 4.0?",
-      options: ["Automation", "AI", "Cyber-Physical Systems", "All of the above"],
-      answer: "All of the above",
+   {
+      question: "Fill in the blank space designated by the question mark.",
+      options: ["Snubber", "Repeater", "Processor", "Amplifier"],
+      answer: "Processor",
+      image: "image.png",
     },
     {
-      question: "Which technology is essential for IoT?",
-      options: ["5G", "Blockchain", "Machine Learning", "All of the above"],
-      answer: "All of the above",
+      question:
+        "2. What is the maximum data rate of Z-Wave communication technology?",
+      options: ["100 kbps", "250 kbps", "500 kbps", "1 Mbps"],
+      answer: "a. 100 kbps",
+    },
+    {
+      question:
+        "Which device converts electromagnetic energy into electricity and vice versa?",
+      options: ["Speaker", "Antenna", "Microphone", "Strain gauge"],
+      answer: "Antenna",
+    },
+    {
+      question: "What does 'Range' indicate in a sensor?",
+      options: [
+        "The correctness of the output",
+        "The smallest change in input that can be detected",
+        "The difference between the standard value and the value produced by the sensor",
+        "The highest and the lowest value of the physical quantity within which the sensor can actually sense",
+      ],
+      answer:
+        "The highest and the lowest value of the physical quantity within which the sensor can actually sense",
+    },
+    {
+      question:
+        "Which coding scheme is used by the IEEE 802.15.4 standard for transmitting information?",
+      options: ["CSS", "THSS", "OFDM", "DSSS"],
+      answer: "DSSS",
+    },
+    {
+      question: "What is the standard transmission range of IEEE 802.15.4?",
+      options: [
+        "10 m to 75 m",
+        "1 m to 9 m",
+        "15 cm to 60 cm",
+        "1 cm to 10 km",
+      ],
+      answer: "10 m to 75 m",
+    },
+    {
+      question:
+        "Which low power radio communication technology was designed as a simpler and cheaper alternative to Zigbee for small to medium range connectivity?",
+      options: ["Bluetooth", "Z-Wave", "RFID", "NFC"],
+      answer: "Z-Wave",
+    },
+    {
+      question: "Which organization has developed the ISA 100.11a standard?",
+      options: [
+        "Instrument Society of America",
+        "International Society of Automation",
+        "International Studies Association",
+        "International Solar Alliance",
+      ],
+      answer: "International Society of Automation",
+    },
+    {
+      question:
+        "What communication modes are supported between two NFC devices?",
+      options: [
+        "Broadcast-Multicast",
+        "Master-Slave",
+        "Active-Active or Active-Passive",
+        "All of these",
+      ],
+      answer: "Active-Active or Active-Passive",
+    },
+    {
+      question:
+        "Which of the following is/are the drawbacks of proprietary non-IP-based solutions?",
+      options: [
+        "Limited flexibility to end users",
+        "Interoperability",
+        "Limited last-mile connectivity",
+        "All of these",
+      ],
+      answer: "All of these",
+    },
+    {
+      question:
+        "Which MQTT QoS Level is also known as “at most once” delivery?",
+      options: ["QoS 0", "QoS 1", "QoS 2", "None of these"],
+      answer: "QoS 0",
+    },
+    {
+      question:
+        "In CoRE, the IoT client uses ________ for the discovery of IoT devices.",
+      options: [
+        "Lookup interface",
+        "Resource Directory",
+        "Registration Interface",
+        "None of these",
+      ],
+      answer: "Lookup interface",
+    },
+    {
+      question:
+        "Apart from MQTT, which IoT networking protocol supports the publish/subscribe framework?",
+      options: ["XMPP", "AMQP", "DDS RTPS", "All of these"],
+      answer: "All of these",
+    },
+    {
+      question:
+        "Which of the following IoT protocols is based on the session layer of the OSI model?",
+      options: ["MQTT", "CoAP", "AMQP", "DTLS"],
+      answer: "DTLS",
+    },
+    {
+      question: "Which of the following is not a component of RFID?",
+      options: ["RFID Tag", "RFID reader", "Antenna", "Router"],
+      answer: "Router",
     },
   ];
   
